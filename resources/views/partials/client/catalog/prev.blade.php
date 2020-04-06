f496ad9e1b934458ce4e17b3b223f76834494df8
Dmytro Haidash <dmyto.haidash@gmail.com>
2020-04-06 10:55:07 +0300

    #

--- resources/views/partials/client/catalog/prev.blade.php
+++ resources/views/partials/client/catalog/prev.blade.php
@@ -2,16 +2,16 @@
     <figure class="lozad teaser__thumbnail"
             data-background-image="{{ $product->getBanner('uploads') }}"></figure>
 
-    <a class="teaser__link p-4" href="{{ route('client.catalog.show', $product) }}">
+    <a class="teaser__link p-2" href="{{ route('client.catalog.show', $product) }}">
         <div class="teaser__title">
-            <h4 class="text-2xl title title--striped">
+            <h4 class="text-xl title title--striped">
                 <span>{{ $product->title }} </span>
             </h4>
         </div>
 
         @if($product->publish_price)
             <div class="flex -mx-2 font-sm">
-                <div class="px-2 w-1/3">{{ $product->price }} @lang('common.currency')</div>
+                <div class="px-2 w-1/3 text-sm">{{ $product->price }} @lang('common.currency')</div>
             </div>
         @endif
     </a>
