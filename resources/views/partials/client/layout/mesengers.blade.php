92bfcd0c2f5a71491ea9e1d971a44c797c47bc24
Dmytro Haidash <dmyto.haidash@gmail.com>
2020-04-27 19:28:09 +0300

    mesengers

--- resources/views/partials/client/layout/mesengers.blade.php
+++ resources/views/partials/client/layout/mesengers.blade.php
@@ -6,14 +6,21 @@
             </svg>
         </a>
     </li>
-    {{--<li>
-        <a href="https://www.instagram.com/"  target="_blank">
+    <li>
+        <a href="https://www.instagram.com/antiquepark.eu/"  target="_blank">
             <svg width="20" height="20">
                 <use xlink:href="#insta"></use>
             </svg>
         </a>
     </li>
     <li>
+        <a href="https://t.me/antiquepark"  target="_blank">
+            <svg width="30" height="30">
+                <use xlink:href="#telegram"></use>
+            </svg>
+        </a>
+    </li>
+    {{--<li>
         <a href="https://www.youtube.com/" target="_blank">
             <svg width="20" height="20">
                 <use xlink:href="#youtube"></use>
